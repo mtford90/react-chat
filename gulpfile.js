a0747ea734332657efdf0e9834d01dc0949f39a9
Michael Ford <mtford@gmail.com>
2014-10-29 16:43:14 -0400

    Finished off scss file watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload'),
+    watch = require('gulp-watch');
 
-gulp.task('scss', function () {
-    gulp.src('./public/styles/scss/*.scss')
+var SCSS_FILES = './public/styles/scss/**/*.scss';
+var CSS_FOLDER = './public/styles/css';
+
+gulp.task('scss', function() {
+    gulp.src(files)
         .pipe(sass())
-        .pipe(gulp.dest('./public/styles/css'));
+        .pipe(gulp.dest(CSS_FOLDER));
 });
 
-
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('watch', function() {
+    gulp.src(SCSS_FILES)
+        .pipe(watch(SCSS_FILES))
+        .pipe(sass())
+        .pipe(gulp.dest(CSS_FOLDER))
+        .pipe(livereload());
 });
