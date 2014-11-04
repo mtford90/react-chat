72a148a572cc7ad5de6f388d9b3aeca248b87028
vitosamson <vslavilla@gmail.com>
2014-11-04 15:22:40 -0500

    add nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     livereload = require('gulp-livereload'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    nodemon = require('nodemon');
 
 var SCSS_FILES = './public/styles/scss/**/*.scss';
 var CSS_FOLDER = './public/styles/css';
@@ -15,6 +16,11 @@ gulp.task('scss', function() {
 });
 
 gulp.task('watch', function() {
+    nodemon({
+        script: 'server.js',
+        ignore: ['public/']
+    });
+
     gulp.src(SCSS_FILES)
         .pipe(watch(SCSS_FILES))
         .pipe(sass())
