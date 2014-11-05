13ff37fb7e5ee26bda007f70ace7f5ec3dd10e30
vitosamson <vslavilla@gmail.com>
2014-11-05 11:36:18 -0500

    cleaned up gulpfile a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     livereload = require('gulp-livereload'),
@@ -9,28 +11,33 @@ var CSS_FOLDER = './public/styles/css';
 var HTML_FILES = './public/**/*.html';
 var JS_FILES = './public/js/**/*.js';
 
-gulp.task('scss', function() {
-    gulp.src(SCSS_FILES)
-        .pipe(sass())
-        .pipe(gulp.dest(CSS_FOLDER));
-});
+gulp.task('watch', ['nodeWatch', 'scssWatch', 'htmlJsWatch']);
 
-gulp.task('watch', function() {
+gulp.task('nodeWatch', function() {
     nodemon({
         script: 'server.js',
-        ignore: ['public/']
+        ignore: ['public/', 'gulpfile.js']
+    })
+    .on('restart', function() {
+        console.log('restarting node server');
+    })
+    .on('crash', function() {
+        console.log('\n!!! node has crashed - will restart after next save !!!');
     });
+});
 
-    gulp.src(SCSS_FILES)
-        .pipe(watch(SCSS_FILES))
-        .pipe(sass())
+gulp.task('scssWatch', function() {
+    watch(SCSS_FILES)
+        .pipe(sass({
+            onError: function(err) {
+                console.log('you call that scss? check out this error: \n', err);
+            }
+        }))
         .pipe(gulp.dest(CSS_FOLDER))
         .pipe(livereload());
+});
 
-    watch(HTML_FILES)
+gulp.task('htmlJsWatch', function() {
+    watch( [HTML_FILES, JS_FILES] )
         .pipe(livereload());
-
-    watch(JS_FILES)
-        .pipe(livereload());
-
 });
